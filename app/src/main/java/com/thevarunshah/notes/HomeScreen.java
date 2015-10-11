969608a5ce342af3dc65ca068e1779c59518de27
Varun Shah <thevarunshah@gmail.com>
2015-10-10 18:40:15 -0700

    added input dialog

--- app/src/main/java/com/thevarunshah/notes/HomeScreen.java
+++ app/src/main/java/com/thevarunshah/notes/HomeScreen.java
@@ -1,9 +1,21 @@
 package com.thevarunshah.notes;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.thevarunshah.backend.NotesAdapter;
 
@@ -24,5 +36,40 @@ public class HomeScreen extends AppCompatActivity {
         }
         RecyclerView.Adapter notesAdapter = new NotesAdapter(sampleData);
         notesList.setAdapter(notesAdapter);
+
+        FloatingActionButton addNote = (FloatingActionButton) findViewById(R.id.add_note);
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater layoutInflater = LayoutInflater.from(HomeScreen.this);
+                final View dialog = layoutInflater.inflate(R.layout.input_dialog, null);
+                final AlertDialog.Builder alertDialog = new AlertDialog.Builder(HomeScreen.this, R.style.AppCompatAlertDialogStyle);
+                alertDialog.setTitle("Note Details");
+
+                alertDialog.setView(dialog);
+
+                final EditText input = (EditText) dialog.findViewById(R.id.input_dialog_text);
+                input.requestFocus();
+                input.setHint("Enter Title");
+
+                alertDialog.setPositiveButton("CREATE", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int whichButton) {
+
+                        String noteTitle = input.getText().toString();
+
+                        RadioGroup noteType = (RadioGroup) dialog.findViewById(R.id.note_type);
+                        int selectedTypeID = noteType.getCheckedRadioButtonId();
+                        RadioButton selectedType = (RadioButton) dialog.findViewById(selectedTypeID);
+                        
+                        Toast.makeText(getApplicationContext(), noteTitle + " - " + selectedType.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertDialog.setNegativeButton("CANCEL", null);
+
+                AlertDialog alert = alertDialog.create();
+                alert.show();
+            }
+        });
     }
 }
