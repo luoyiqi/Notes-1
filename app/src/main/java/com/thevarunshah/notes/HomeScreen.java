18007742c704a3d96daa38fec890da29c74bf664
Varun Shah <thevarunshah@gmail.com>
2016-01-02 22:39:53 -0500

    updated input dialog to use less screen space

--- app/src/main/java/com/thevarunshah/notes/HomeScreen.java
+++ app/src/main/java/com/thevarunshah/notes/HomeScreen.java
@@ -11,14 +11,16 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.RadioGroup;
+import android.widget.RadioButton;
 
-import com.thevarunshah.classes.Checklist;
 import com.thevarunshah.backend.Backend;
-import com.thevarunshah.classes.ListNote;
 import com.thevarunshah.backend.NoteAdapter;
 import com.thevarunshah.backend.RecyclerItemClickListener;
+import com.thevarunshah.classes.Checklist;
+import com.thevarunshah.classes.ListNote;
 import com.thevarunshah.classes.Reminder;
 import com.thevarunshah.classes.TextNote;
 
@@ -41,28 +43,25 @@ public class HomeScreen extends AppCompatActivity {
         notesList.setAdapter(notesAdapter);
 
         notesList.addOnItemTouchListener(
-            new RecyclerItemClickListener(HomeScreen.this, new RecyclerItemClickListener.OnItemClickListener() {
-                @Override
-                public void onItemClick(View view, int position) {
-                    Object note = notesAdapter.getItem(position);
-                    if(note instanceof TextNote){
-                        TextNote tn = (TextNote) note;
-                        Log.i(TAG, tn.getName());
+                new RecyclerItemClickListener(HomeScreen.this, new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Object note = notesAdapter.getItem(position);
+                        if (note instanceof TextNote) {
+                            TextNote tn = (TextNote) note;
+                            Log.i(TAG, tn.getName());
+                        } else if (note instanceof ListNote) {
+                            ListNote ln = (ListNote) note;
+                            Log.i(TAG, ln.getName());
+                        } else if (note instanceof Checklist) {
+                            Checklist cl = (Checklist) note;
+                            Log.i(TAG, cl.getName());
+                        } else if (note instanceof Reminder) {
+                            Reminder r = (Reminder) note;
+                            Log.i(TAG, r.getName());
+                        }
                     }
-                    else if(note instanceof ListNote){
-                        ListNote ln = (ListNote) note;
-                        Log.i(TAG, ln.getName());
-                    }
-                    else if(note instanceof Checklist){
-                        Checklist cl = (Checklist) note;
-                        Log.i(TAG, cl.getName());
-                    }
-                    else if(note instanceof Reminder){
-                        Reminder r = (Reminder) note;
-                        Log.i(TAG, r.getName());
-                    }
-                }
-            })
+                })
         );
 
         FloatingActionButton addNote = (FloatingActionButton) findViewById(R.id.add_note);
@@ -76,36 +75,86 @@ public class HomeScreen extends AppCompatActivity {
                         R.style.AppCompatAlertDialogStyle);
                 alertDialog.setTitle("New Note");
 
+                final RadioButton tnButton = (RadioButton) dialog.findViewById(R.id.text_note);
+                final RadioButton lnButton = (RadioButton) dialog.findViewById(R.id.list_note);
+                final RadioButton clButton = (RadioButton) dialog.findViewById(R.id.checklist);
+                final RadioButton rButton = (RadioButton) dialog.findViewById(R.id.reminder);
+                tnButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                        if(!isChecked){
+                            return;
+                        }
+                        lnButton.setChecked(false);
+                        clButton.setChecked(false);
+                        rButton.setChecked(false);
+                    }
+                });
+                lnButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                        if(!isChecked){
+                            return;
+                        }
+                        tnButton.setChecked(false);
+                        clButton.setChecked(false);
+                        rButton.setChecked(false);
+                    }
+                });
+                clButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                        if(!isChecked){
+                            return;
+                        }
+                        tnButton.setChecked(false);
+                        lnButton.setChecked(false);
+                        rButton.setChecked(false);
+                    }
+                });
+                rButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                        if (!isChecked) {
+                            return;
+                        }
+                        tnButton.setChecked(false);
+                        lnButton.setChecked(false);
+                        clButton.setChecked(false);
+                    }
+                });
+
                 alertDialog.setView(dialog);
 
                 final EditText input = (EditText) dialog.findViewById(R.id.input_dialog_text);
-                input.requestFocus();
                 input.setHint("Enter Title");
+                input.setFocusableInTouchMode(true);
+                input.requestFocus();
 
                 alertDialog.setPositiveButton("CREATE", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialogInterface, int whichButton) {
 
                         String noteTitle = input.getText().toString();
 
-                        RadioGroup noteType = (RadioGroup) dialog.findViewById(R.id.note_type);
-                        int selectedTypeID = noteType.getCheckedRadioButtonId();
-                        switch (selectedTypeID) {
-                            case R.id.text_note:
-                                TextNote tn = new TextNote(Backend.getNextID(), noteTitle);
-                                notesAdapter.add(tn);
-                                break;
-                            case R.id.list_note:
-                                ListNote ln = new ListNote(Backend.getNextID(), noteTitle);
-                                notesAdapter.add(ln);
-                                break;
-                            case R.id.checklist:
-                                Checklist cl = new Checklist(Backend.getNextID(), noteTitle);
-                                notesAdapter.add(cl);
-                                break;
-                            case R.id.reminder:
-                                Reminder r = new Reminder(Backend.getNextID(), noteTitle);
-                                notesAdapter.add(r);
-                                break;
+                        if(tnButton.isChecked()){
+                            TextNote tn = new TextNote(Backend.getNextID(), noteTitle);
+                            notesAdapter.add(tn);
+                        }
+                        else if(lnButton.isChecked()){
+                            ListNote ln = new ListNote(Backend.getNextID(), noteTitle);
+                            notesAdapter.add(ln);
+                        }
+                        else if(clButton.isChecked()){
+                            Checklist cl = new Checklist(Backend.getNextID(), noteTitle);
+                            notesAdapter.add(cl);
+                        }
+                        else if(rButton.isChecked()){
+                            Reminder r = new Reminder(Backend.getNextID(), noteTitle);
+                            notesAdapter.add(r);
                         }
                     }
                 });
@@ -113,6 +162,8 @@ public class HomeScreen extends AppCompatActivity {
 
                 AlertDialog alert = alertDialog.create();
                 alert.show();
+
+                alert.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
             }
         });
     }
