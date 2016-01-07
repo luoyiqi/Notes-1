8edc32fbe35c92149af3db576ed60da44829a2da
Varun Shah <thevarunshah@gmail.com>
2016-01-06 22:45:26 -0500

    updated main list view to include note type

--- app/src/main/java/com/thevarunshah/backend/NoteAdapter.java
+++ app/src/main/java/com/thevarunshah/backend/NoteAdapter.java
@@ -1,13 +1,22 @@
 package com.thevarunshah.backend;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.thevarunshah.classes.Checklist;
+import com.thevarunshah.classes.ListNote;
 import com.thevarunshah.classes.Note;
+import com.thevarunshah.classes.Reminder;
+import com.thevarunshah.classes.TextNote;
+import com.thevarunshah.notes.HomeScreen;
 import com.thevarunshah.notes.R;
+import com.thevarunshah.notes.TextNoteView;
 
 import java.util.List;
 
@@ -32,15 +41,30 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder>{
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
 
         viewHolder.txtViewTitle.setText(items.get(position).getName());
+        Object note = items.get(position);
+        if(note instanceof TextNote){
+            viewHolder.txtViewType.setText("Text");
+        }
+        else if(note instanceof ListNote){
+            viewHolder.txtViewType.setText("List");
+        }
+        else if(note instanceof Checklist){
+            viewHolder.txtViewType.setText("Checklist");
+        }
+        else if(note instanceof Reminder){
+            viewHolder.txtViewType.setText("Reminder");
+        }
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView txtViewTitle;
+        public TextView txtViewType;
 
         public ViewHolder(View itemLayoutView) {
             super(itemLayoutView);
             txtViewTitle = (TextView) itemLayoutView.findViewById(R.id.notes_list_textview);
+            txtViewType = (TextView) itemLayoutView.findViewById(R.id.notes_list_type_textview);
         }
     }
 
